c39c9d42c72c211c22f80eb3b3dac5d5f37f4a6c
simon <simonmontenegro55@gmail.com>
2025-11-17 18:58:35 -0300

    Agrega Entidad de dom Familia

--- src/Core/Domain/Entities/Categoria.cs
+++ src/Core/Domain/Entities/Categoria.cs
@@ -5,11 +5,6 @@ public class Categoria
     public int IdCategoria { get; init; }
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
-    public Categoria(int idCategoria, string nombre, string descripcion)
-    {
-        IdCategoria = idCategoria;
-        Nombre = nombre;
-        Descripcion = descripcion;
-    }
+    public Familia Familia { get; set; }
     public Categoria() { }
 }
