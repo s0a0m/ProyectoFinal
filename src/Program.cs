ea86dcf0b8737a7319b6e360a3960bd04bf942dd
simon <simonmontenegro55@gmail.com>
2025-11-20 21:00:43 -0300

    Agrega ID para novedades repo, service

--- src/Program.cs
+++ src/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddScoped<IBarcodeAdapter, ZxIngBarcodeAdapter>();
 builder.Services.AddScoped<IProductoCodigoExternoRepository, ProductoCodigoExternoRepository>();
 builder.Services.AddScoped<IProductoProveedorRepository, ProductoProveedorRepository>();
 builder.Services.AddScoped<IExcelDataReader, ExcelDataAdapter>();
+builder.Services.AddScoped<INovedadesRepository, NovedadesRepository>();
 
 // Servicios
 builder.Services.AddScoped<IUserService, UserService>();
