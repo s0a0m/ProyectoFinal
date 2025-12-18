10279ad12afa02fc7b7e7e5860190c3f154b19cd
simon <simonmontenegro55@gmail.com>
2025-12-18 14:05:24 -0300

    Crea migracion para auditoria compras

---
+++ src/Infrastructure/Data/Migrations/20251218170443_CrearTablaAuditoriaCompras.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace src.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearTablaAuditoriaCompras : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "auditoria_compras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdCompra = table.Column<int>(type: "integer", nullable: false),
+                    Usuario = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Accion = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Detalles = table.Column<string>(type: "text", nullable: false),
+                    Motivo = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_auditoria_compras", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "auditoria_compras");
+        }
+    }
+}
