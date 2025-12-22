f02519d6c87bb69539193e77195f6451bf115849
simon <simonmontenegro55@gmail.com>
2025-12-22 15:56:37 -0300

    Fix de tipos de datos en factura

--- src/Presentation/ViewModels/FacturaVM/ActualizarFacturaViewModel.cs
+++ src/Presentation/ViewModels/FacturaVM/ActualizarFacturaViewModel.cs
@@ -6,7 +6,7 @@ namespace src.Presentation.ViewModels.FacturaVM
     {
         public string NumeroFactura { get; set; } = null!;
         public DateTime FechaEmision { get; set; }
-        public int IdCondicionPago { get; set; }
+        public short IdCondicionPago { get; set; }
         public bool Pagada { get; set; }
     }
 }
